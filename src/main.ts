b5ca4663b0c182f0fd173a315b2866fec21463b5
XATTAP <cherednikovkirill@gmail.com>
2024-07-31 16:35:27 +0300

    add env file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT || 3000);
+  console.log(`server start on host: http://localhost:${process.env.SERVER_PORT || 3000}`)
 }
 bootstrap();
